d19adf56ac415be25e04e39fcbaf95c796748d61
21 Tech Enterprise <>
2024-03-03 11:25:59 +0700

    done

--- app/components/HomeNew/CallToAction.tsx
+++ app/components/HomeNew/CallToAction.tsx
@@ -5,13 +5,13 @@ const CallToAction = () => {
         <div className="w-full h-[74px] fixed bottom-[24px] px-[30px] z-[999] md:hidden">
             <div className="w-[100%] h-[100%] rounded-full">
                 <div className="grid grid-cols-3 h-[100%]">
-                    <a href="tel://0625399395" target="_blank" className="flex items-center justify-center bg-slate-400 rounded-l-full">
+                    <a href="tel://0994569591" target="_blank" className="flex items-center justify-center bg-slate-400 rounded-l-full">
                         <BsFillTelephoneFill style={{ fontSize: 30, color: 'white' }} />
                     </a>
-                    <a href="https://line.me/ti/p/~Smiley_season" target="_blank" className="flex items-center justify-center  bg-[#00b900]">
+                    <a href="https://line.me/ti/p/~nookmayz" target="_blank" className="flex items-center justify-center  bg-[#00b900]">
                         <BsLine style={{ fontSize: 35, color: 'white' }} />
                     </a>
-                    <a href="https://www.facebook.com/profile.php/?id=61553635442923" target="_blank" className="flex items-center justify-center  bg-[#3c5a99] rounded-r-full">
+                    <a href="https://www.facebook.com/experts8academy" target="_blank" className="flex items-center justify-center  bg-[#3c5a99] rounded-r-full">
                         <BsFacebook style={{ fontSize: 35, color: 'white' }} />
                     </a>
                 </div>
