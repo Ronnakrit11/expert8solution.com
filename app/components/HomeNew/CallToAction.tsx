1a206c3bad5807952c17bf429e797abfd30251bb
21 Tech Enterprise <>
2024-03-12 23:49:05 +0700

    done

--- app/components/HomeNew/CallToAction.tsx
+++ app/components/HomeNew/CallToAction.tsx
@@ -8,7 +8,7 @@ const CallToAction = () => {
                     <a href="tel://0994569591" target="_blank" className="flex items-center justify-center bg-slate-400 rounded-l-full">
                         <BsFillTelephoneFill style={{ fontSize: 30, color: 'white' }} />
                     </a>
-                    <a href="https://line.me/ti/p/~nookmayz" target="_blank" className="flex items-center justify-center  bg-[#00b900]">
+                    <a href="https://line.me/ti/p/5N4dECXfqL" target="_blank" className="flex items-center justify-center  bg-[#00b900]">
                         <BsLine style={{ fontSize: 35, color: 'white' }} />
                     </a>
                     <a href="https://www.facebook.com/experts8academy" target="_blank" className="flex items-center justify-center  bg-[#3c5a99] rounded-r-full">
