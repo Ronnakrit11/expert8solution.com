751b86e8dd3c608356b67c71974bcf3b3711ccf7
21 Tech Enterprise <>
2024-04-06 16:43:41 +0700

    done

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -100,7 +100,7 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
                       className={`text-[18px] md:text-[25px] flex items-center font-Poppins font-[500] text-black dark:text-white`}
                     >
                       <Image
-                        src={'/logonew2.png'}
+                        src={'/exlogo.png'}
                         width={230}
                         height={250}
                         alt=""
