61b041435a3fdbcc16af5dd3d988f3404937ac33
21 Tech Enterprise <>
2024-04-11 15:59:21 +0700

    done

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://primeschool.online/',
+      url: 'https://www.expert8-solution.com/',
       lastModified: new Date(),
     },
     {
-      url: 'https://primeschool.online/courses',
+      url: 'https://www.expert8-solution.com/courses',
       lastModified: new Date(),
     },
     {
-      url: 'https://primeschool.online/about',
+      url: 'https://www.expert8-solution.com/blog',
       lastModified: new Date(),
     },
     {
-      url: 'https://primeschool.online/ebook',
+      url: 'https://www.expert8-solution.com/about',
       lastModified: new Date(),
     },
   ]
