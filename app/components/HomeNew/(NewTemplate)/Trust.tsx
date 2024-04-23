af0dd7946829408ef23a77b51b0749d5dac6793e
Nontawach Wiangkom <babydev.weangkom@gmail.com>
2024-04-24 00:03:33 +0700

    Update Trust.tsx

--- app/components/HomeNew/(NewTemplate)/Trust.tsx
+++ app/components/HomeNew/(NewTemplate)/Trust.tsx
@@ -3,26 +3,26 @@ import React from 'react'
 const Trust = () => {
     const item = [
         {
-            img: "E-Lernning"
+            img: "https://ampire.tailus.io/images/clients/airbnb.svg"
         },
         {
-            img: "App Shop"
+            img: "https://ampire.tailus.io/images/clients/coty.svg"
         },
         {
-            img: "E-commerce"
+            img: "https://ampire.tailus.io/images/clients/ge.svg"
         },
         {
-            img: "Add"
+            img: "https://ampire.tailus.io/images/clients/lilly.png"
         },
         {
-            img: "Graphic"
+            img: "https://ampire.tailus.io/images/clients/microsoft.svg"
         }
     ]
     return (
         <div className='container mx-auto mt-12 border-y border-gray-100 py-8 text-center dark:border-gray-800 md:py-12 xl:mt-20'>
             <div className='mt-8 flex flex-wrap justify-center gap-6 brightness-75 contrast-200 dark:brightness-200 dark:contrast-0 sm:justify-between lg:gap-24'>
                 {item.map((item, index) => (
-                    <p className='text-xl text-sky-600 font-semibold '>{item.img}</p>
+                    <img src={item.img} key={index} className='h-8 w-auto lg:h-10 lg:w-auto' />
                 ))}
             </div>
         </div>
