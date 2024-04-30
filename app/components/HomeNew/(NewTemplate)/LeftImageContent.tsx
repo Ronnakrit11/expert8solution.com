6adc25b7c858bbcb07b3bcc729900959a2804dc2
Nontawach Wiangkom <babydev.weangkom@gmail.com>
2024-05-01 01:18:04 +0700

    change Image

--- app/components/HomeNew/(NewTemplate)/LeftImageContent.tsx
+++ app/components/HomeNew/(NewTemplate)/LeftImageContent.tsx
@@ -8,7 +8,7 @@ const LeftImageContent = () => {
             <div className='container mx-auto mp-32'>
                 <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2'>
                     <div>
-                        <Image width={500} height={500} alt='' src='/image1.jpg' className='p-8' />
+                        <Image width={500} height={500} alt='' src='/imageleft.webp' className='p-8' />
                     </div>
                     <div className='flex flex-col justify-center'>
                         <p className='text-3xl font-bold text-gray-800 dark:text-white md:text-4xl xl:text-5xl'>Development is carried out by passionate developers</p>
