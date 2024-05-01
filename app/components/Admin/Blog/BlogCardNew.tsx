a81bdb294baa16a019edb706315c08aced8568c0
Nontawach Wiangkom <babydev.weangkom@gmail.com>
2024-05-01 09:34:39 +0700

    Add Datetime Card Blog

--- app/components/Admin/Blog/BlogCardNew.tsx
+++ app/components/Admin/Blog/BlogCardNew.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import React, { FC } from "react";
 import { AiOutlineUnorderedList } from "react-icons/ai";
 import { useRouter } from 'next/navigation'
+import dayjs from "dayjs";
+import "dayjs/locale/th";
 
 type Props = {
     item: any;
@@ -13,18 +15,19 @@ type Props = {
 const BlogCardNew: FC<Props> = ({ item, isProfile }) => {
     const router = useRouter()
     const linkUrl = `/blog/${item.slug}`
-
+    dayjs.locale("th");
     const handleClick = (e: any) => {
         e.preventDefault()
         router.push(linkUrl)
     }
-    console.log(isProfile)
+    console.log(item.createdAt)
     return (
         <>
-            <a onClick={handleClick} className="block cursor-pointer rounded-2xl px-4 py-6 sm:border bg-gray-200 dark:sm:border-gray-800 sm:dark:bg-gray-800/50">
+            <a onClick={handleClick} className="block cursor-pointer rounded-2xl px-4 py-6 sm:border bg-gray-200 dark:bg-darkbg dark:sm:border-gray-800 sm:dark:bg-gray-800/50">
                 <img className="rounded-3xl" src={item.thumbnail.url} alt="blog cover" width="1556" height="778" />
                 <div className="mt-8">
                     <h2 className="text-2xl font-bold text-gray-800 dark:text-white md:text-3xl">{item.title}</h2>
+                    <p className="text-white">{dayjs(item.createdAt).format("DD MMMM YYYY")}</p>
                     <p className="mt-4 text-gray-700 dark:text-gray-300">{add3Dots(item.description, 100)}</p>
                     {/*<div className='flex items-center gap-3 mt-8'>
                         <img className="h-12 w-12 rounded-full" src="https://ampire.tailus.io/images/avatars/avatar-2.webp" loading="lazy" alt="user avatar" width="200" height="200" />
