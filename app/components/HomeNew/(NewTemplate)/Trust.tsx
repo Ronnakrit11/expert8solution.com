c389a0469d241685beeeacece39f4f576325970c
21 Tech Enterprise <>
2024-05-03 22:18:27 +0700

    done

--- app/components/HomeNew/(NewTemplate)/Trust.tsx
+++ app/components/HomeNew/(NewTemplate)/Trust.tsx
@@ -36,10 +36,14 @@ const Trust = () => {
         }
     ];
     return (
-        <div className='bg-white dark:bg-[#101726] pt-12 p-4'>
-            <div className='container mx-auto border-y border-gray-100 py-8 text-center dark:border-gray-800  md:py-12 xl:mt-20'>
-                <div className='mt-8 flex flex-wrap justify-center gap-6 brightness-75 contrast-200 dark:brightness-200 dark:contrast-0 sm:justify-between lg:gap-24'>
+        <div className='bg-white dark:bg-[#101726] pt-12 p-4 '>
+     
+            <div className='container mx-auto border-y border-gray-100 py-8 text-center dark:border-gray-800  md:py-12 xl:mt-15 '>
+                <div className='mt-4 flex flex-wrap justify-center gap-6 brightness-75 contrast-200 dark:brightness-200 dark:contrast-0 sm:justify-between lg:gap-24'>
+               
+
                     {items.map((item, index) => (
+                        
                         <div key={index} className='h-15 w-auto lg:h-17 lg:w-auto'>
                             <Image
                                 src={item.img}
