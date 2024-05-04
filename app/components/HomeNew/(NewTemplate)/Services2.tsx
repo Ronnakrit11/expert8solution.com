d650a2e9cc1828c5c5e51f24cc11b286146703a1
babydev <161356967+babydepdev@users.noreply.github.com>
2024-05-04 12:39:46 +0700

    Update Services2.tsx

--- app/components/HomeNew/(NewTemplate)/Services2.tsx
+++ app/components/HomeNew/(NewTemplate)/Services2.tsx
@@ -2,63 +2,39 @@ import React from 'react'
 
 const Services2 = () => {
     return (
-        <section className='bg-white'>
+        <section className='bg-white dark:bg-darkbg'>
             <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-col items-start space-y-6 lg:space-y-8 xl:space-y-10">
-                    <div className="bg-white shadow-sm ring-1 ring-inset ring-gray-200 rounded-3xl xl:sticky xl:top-20">
+                    <div className="bg-white dark:bg-darkbg shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
                         <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
                             <div className="p-8 sm:p-12 lg:p-16">
-                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600">Personalized Chatbot</p>
-                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">Build a custom chatbot trained on your own content</h3>
-                                <p className="text-base font-normal text-gray-700 mt-4">Ever wanted to clone yourself to answer customer queries? Now you can! Train your chatbot with your content and let it echo your brand's voice. Who knew a chatbot could be your digital doppelgänger?</p>
+                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-gray-300">Personalized Chatbot</p>
+                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">Build a custom chatbot trained on your own content</h3>
+                                <p className="text-base font-normal text-gray-700 dark:text-gray-500 mt-4">Ever wanted to clone yourself to answer customer queries? Now you can! Train your chatbot with your content and let it echo your brand's voice. Who knew a chatbot could be your digital doppelgänger?</p>
                             </div>
                             <div>
                                 <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
                             </div>
                         </div>
                     </div>
-                    <div className="bg-white shadow-sm ring-1 ring-inset ring-gray-200 rounded-3xl xl:sticky xl:top-20">
+                    <div className="bg-white dark:bg-darkbg shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
                         <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
                             <div className="p-8 sm:p-12 lg:p-16">
-                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600">Personalized Chatbot</p>
-                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">Build a custom chatbot trained on your own content</h3>
-                                <p className="text-base font-normal text-gray-700 mt-4">Ever wanted to clone yourself to answer customer queries? Now you can! Train your chatbot with your content and let it echo your brand's voice. Who knew a chatbot could be your digital doppelgänger?</p>
+                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-gray-300">Personalized Chatbot</p>
+                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">Build a custom chatbot trained on your own content</h3>
+                                <p className="text-base font-normal text-gray-700 dark:text-gray-500 mt-4">Ever wanted to clone yourself to answer customer queries? Now you can! Train your chatbot with your content and let it echo your brand's voice. Who knew a chatbot could be your digital doppelgänger?</p>
                             </div>
                             <div>
                                 <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
                             </div>
                         </div>
                     </div>
-                    <div className="bg-white shadow-sm ring-1 ring-inset ring-gray-200 rounded-3xl xl:sticky xl:top-20">
+                    <div className="bg-white dark:bg-darkbg shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
                         <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
                             <div className="p-8 sm:p-12 lg:p-16">
-                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600">Personalized Chatbot</p>
-                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">Build a custom chatbot trained on your own content</h3>
-                                <p className="text-base font-normal text-gray-700 mt-4">Ever wanted to clone yourself to answer customer queries? Now you can! Train your chatbot with your content and let it echo your brand's voice. Who knew a chatbot could be your digital doppelgänger?</p>
-                            </div>
-                            <div>
-                                <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="bg-white shadow-sm ring-1 ring-inset ring-gray-200 rounded-3xl xl:sticky xl:top-20">
-                        <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
-                            <div className="p-8 sm:p-12 lg:p-16">
-                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600">Personalized Chatbot</p>
-                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">Build a custom chatbot trained on your own content</h3>
-                                <p className="text-base font-normal text-gray-700 mt-4">Ever wanted to clone yourself to answer customer queries? Now you can! Train your chatbot with your content and let it echo your brand's voice. Who knew a chatbot could be your digital doppelgänger?</p>
-                            </div>
-                            <div>
-                                <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="bg-white shadow-sm ring-1 ring-inset ring-gray-200 rounded-3xl xl:sticky xl:top-20">
-                        <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
-                            <div className="p-8 sm:p-12 lg:p-16">
-                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600">Personalized Chatbot</p>
-                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">Build a custom chatbot trained on your own content</h3>
-                                <p className="text-base font-normal text-gray-700 mt-4">Ever wanted to clone yourself to answer customer queries? Now you can! Train your chatbot with your content and let it echo your brand's voice. Who knew a chatbot could be your digital doppelgänger?</p>
+                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-gray-300">Personalized Chatbot</p>
+                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">Build a custom chatbot trained on your own content</h3>
+                                <p className="text-base font-normal text-gray-700 dark:text-gray-500 mt-4">Ever wanted to clone yourself to answer customer queries? Now you can! Train your chatbot with your content and let it echo your brand's voice. Who knew a chatbot could be your digital doppelgänger?</p>
                             </div>
                             <div>
                                 <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
