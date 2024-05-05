09e1805c486ce3c0223e370949fb626eb5ca9866
babydev <161356967+babydepdev@users.noreply.github.com>
2024-05-05 17:20:59 +0700

    Update Services2.tsx

--- app/components/HomeNew/(NewTemplate)/Services2.tsx
+++ app/components/HomeNew/(NewTemplate)/Services2.tsx
@@ -4,19 +4,19 @@ const Services2 = () => {
     return (
         <section className='bg-white dark:bg-darkbg'>
             <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="bg-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
-                    <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
-                        <div className="p-8 sm:p-12 lg:p-16">
-                            <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-white">ความสะดวกสบาย</p>
-                            <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">ดีไซน์สวยงาม ทันสมัย ใช้งานง่าย</h2>
-                            <p className="text-base font-normal text-black dark:text-white mt-4">เราคำนึงถึงลูกค้าเป็นหลัก เพียงไม่กี่คลิ๊กนักเรียนสามารถเข้าสู่ระบบเเละจ่ายเงินเข้าเรียนได้ทันที ระบบหลังบ้านใช้งานง่าย</p>
-                        </div>
-                        <div>
-                            <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
+                <div className="flex flex-col items-start space-y-6 lg:space-y-8 xl:space-y-10">
+                    <div className="bg-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
+                        <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
+                            <div className="p-8 sm:p-12 lg:p-16">
+                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-white">ความสะดวกสบาย</p>
+                                <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">ดีไซน์สวยงาม ทันสมัย ใช้งานง่าย</h2>
+                                <p className="text-base font-normal text-black dark:text-white mt-4">เราคำนึงถึงลูกค้าเป็นหลัก เพียงไม่กี่คลิ๊กนักเรียนสามารถเข้าสู่ระบบเเละจ่ายเงินเข้าเรียนได้ทันที ระบบหลังบ้านใช้งานง่าย</p>
+                            </div>
+                            <div>
+                                <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="flex flex-col items-start space-y-6 lg:space-y-8 xl:space-y-10">
                     <div className="bg-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
                         <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
                             <div className="p-8 sm:p-12 lg:p-16">
