cbdc9af7570455963ef1c66671f5320db100ff3f
21 Tech Enterprise <>
2024-05-05 13:55:43 +0700

    dome

--- app/components/HomeNew/(NewTemplate)/Services2.tsx
+++ app/components/HomeNew/(NewTemplate)/Services2.tsx
@@ -4,13 +4,51 @@ const Services2 = () => {
     return (
         <section className='bg-white dark:bg-darkbg'>
             <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="bg-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
+                    <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
+                        <div className="p-8 sm:p-12 lg:p-16">
+                            <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-white">ความสะดวกสบาย</p>
+                            <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">ดีไซน์สวยงาม ทันสมัย ใช้งานง่ายทั้งฝั่งของนักเรียน เเละเจ้าของเว็บไซต์</h2>
+                            <p className="text-base font-normal text-black dark:text-white mt-4">เราคำนึงถึงลูกค้าเป็นหลัก เพียงไม่กี่คลิ๊กนักเรียนสามารถเข้าสู่ระบบเเละจ่ายเงินเข้าเร่ียนได้ทันที </p>
+                        </div>
+                        <div>
+                            <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
+                        </div>
+                    </div>
+                </div>
                 <div className="flex flex-col items-start space-y-6 lg:space-y-8 xl:space-y-10">
                     <div className="bg-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
                         <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
                             <div className="p-8 sm:p-12 lg:p-16">
-                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-white">ระบบยืดหยุ่น</p>
-                                <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">ระบบเรียนออนไลน์ของลูกค้าเเบบตรงความต้องการทุกอย่าง</h2>
-                                <p className="text-base font-normal text-black dark:text-white mt-4">ระบบเรียนออนไลน์ของเราพัฒนาด้วยภาษา Programming ที่ดีที่สุด เพื่อประสิทธิภาพสูงสุดของ User</p>
+                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-white">ครอบคลุม</p>
+                                <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">มีระบบชำระเงินที่ครบครัน</h2>
+                                <p className="text-base font-normal text-black dark:text-white mt-4">ระบบเรียนออนไลน์ของเราสามารถรองรับการชำระเงินทุกช่องทาง บัตรเครดิต บัตรเดบิต ผ่อนชำระ รวมถึง True money wallet</p>
+                            </div>
+                            <div>
+                                <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bg-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
+                        <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
+                            <div className="p-8 sm:p-12 lg:p-16">
+                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-white">เพิ่มช่องทางรายได้</p>
+                                <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">ระบบเรียนออนไลน์ของลูกค้าขายได้ทั้งคอร์สเรียนเเละอีบุ๊ค</h2>
+                                <p className="text-base font-normal text-black dark:text-white mt-4">เราคำนึงถึงสิทธิประโยชน์ของลูกค้าเป็นหลัก ด้วยการเพิ่มฟังก์การขายอีบุ๊คเข้าไป</p>
+                            </div>
+                            <div>
+                                <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bg-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
+                        <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
+                            <div className="p-8 sm:p-12 lg:p-16">
+                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-white">Security</p>
+                                <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">น่าเชื่อถือ ปลอดภัย</h2>
+                                <p className="text-base font-normal text-black dark:text-white mt-4">ระบบพัฒนาด้วยภาษา next js ซึ่งมีประสิทธิภาพเเละมีการป้องกันที่เเน่นหนา</p>
                             </div>
                             <div>
                                 <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
@@ -19,40 +57,17 @@ const Services2 = () => {
                     </div>
                     <div className="bg-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
                         <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
-                        <div className="p-8 sm:p-12 lg:p-16">
+                            <div className="p-8 sm:p-12 lg:p-16">
                                 <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-white">ภาพรวมธุรกิจ</p>
                                 <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">เราเข้าใจภาพรวมของธุรกิจคอร์สเรียนออนไลน์</h2>
-                                <p className="text-base font-normal text-black dark:text-white mt-4">เนื่องจากทางเราได้ทำเว็บไซต์สำหรับการเรียนออนไลน์เหมือนกัน ทำให้เข้าใจในมุมมองของธุรกิจขายคอร์สออนไลน์ดี</p>
+                                <p className="text-base font-normal text-black dark:text-white mt-4">เนื่องจากทางเราได้ทำเว็บไซต์สำหรับการเรียนออนไลน์เหมือนกัน ทำให้เข้าใจในมุมมองของธุรกิจขายคอร์สออนไลน์ทั้งในส่วนเจ้าของเว็บไซต์เเละส่วนของนักเรียน</p>
                             </div>
                             <div>
                                 <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
                             </div>
                         </div>
                     </div>
-                    <div className="bg-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
-                        <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
-                        <div className="p-8 sm:p-12 lg:p-16">
-                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-white">การใช้งานที่ง่ายดาย</p>
-                                <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">ระบบเรียนออนไลน์ของลูกค้าใช้งานได้ง่ายดาย</h2>
-                                <p className="text-base font-normal text-black dark:text-white mt-4">เพียงไม่กี่คลิ๊กนักเรียนก็สามารถจ่ายเงิน เเละระบบหลังบ้านใช้งานง่าย</p>
-                            </div>
-                            <div>
-                                <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="bg-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-200 dark:ring-gray-800 rounded-3xl xl:sticky xl:top-20">
-                        <div className="grid grid-cols-1 items-center -space-y-12 lg:grid-cols-2 lg:space-y-0">
-                        <div className="p-8 sm:p-12 lg:p-16">
-                                <p className="text-sm font-bold uppercase tracking-wide text-blue-600 dark:text-white">ฟังก์ชั่นที่ครบครัน</p>
-                                <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4 sm:text-3xl sm:mt-6 lg:text-4xl">ระบบเรียนออนไลน์ของลูกค้าขายได้ทั้งคอร์สเรียนเเละอีบุ๊ค</h2>
-                                <p className="text-base font-normal text-black dark:text-white mt-4">ตอบโจทย์การขายทุกอย่าง เพิ่มช่องทางการขายได้มากขึ้น</p>
-                            </div>
-                            <div>
-                                <img src='https://sitegpt.ai/images/features/feature-1.svg' className='w-full object-cover' />
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </section>
