a86e99081ee883aa8478bfd77dc8b0ceea462e3a
21 Tech Enterprise <>
2024-05-16 11:13:33 +0700

    done

--- app/components/HomeNew/(NewTemplate)/LandingPage.tsx
+++ app/components/HomeNew/(NewTemplate)/LandingPage.tsx
@@ -12,7 +12,7 @@ const LandingPage = () => {
                             <h1 className='text-center text-5xl font-bold text-blue-900 dark:text-white sm:text-6xl lg:text-left lg:text-7xl'>Expert 8 &nbsp;</h1>
                             <h1 className='text-5xl sm:text-6xl lg:text-left lg:text-7xl font-bold bg-gradient-to-r from-blue-600 via-sky-500 to-indigo-400 inline-block text-transparent bg-clip-text'>Solution.</h1>
                         </div>
-                        <h2 className='my-6 lg:my-12 text-lg text-gray-600 dark:text-gray-300 sm:text-xl'>บริการสร้างระบบ E-Learning เเบบครบครัน พร้อมบริการงานกราฟฟิก งานตัดต่อ เเละเทคนิคยิงโฆษณาโดยมืออาชีพ</h2>
+                        <h2 className='my-6 lg:my-12 text-lg text-gray-600 dark:text-gray-300 sm:text-xl'>บริการจัดทำเว็ปไซต์ E-learning เเพลทฟอร์มการเรียนออนไลน์ครบวงจร เเละซอฟเเวร์ทุกประเภท </h2>
                         <div className='relative flex items-center rounded-full border border-primary/20 bg-white dark:bg-[#101726] p-1 px-2 shadow-md focus-within:ring-2 dark:border-white/10 dark:bg-dark dark:text-white md:p-2 lg:pr-3'>
                             <FaEnvelope className='h-6 w-6 ml-2 text-gray-600' />
                             <input placeholder='Your mail address' className='w-full rounded-full bg-transparent p-4 placeholder-gray-600  outline-none dark:placeholder-white' />
