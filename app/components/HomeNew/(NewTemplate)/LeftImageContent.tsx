70efc4b91e84448385d8fd7dc05a68113ae65669
21 Tech Enterprise <59superrichsol@gmail.com>
2024-05-30 09:24:16 +0700

    done

--- app/components/HomeNew/(NewTemplate)/LeftImageContent.tsx
+++ app/components/HomeNew/(NewTemplate)/LeftImageContent.tsx
@@ -12,8 +12,8 @@ const LeftImageContent = () => {
                     </div>
                     <div className='flex flex-col justify-center'>
                         <h2 className='text-3xl font-bold text-gray-800 dark:text-white md:text-4xl xl:text-5xl'>เพราะเราเข้าใจในธุรกิจการเรียนออนไลน์</h2>
-                        <p className='mt-8 text-gray-600 dark:text-gray-300'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
-                        <p className='mb-12 mt-4 text-gray-600 dark:text-gray-300'>Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
+                        <p className='mt-8 text-gray-600 dark:text-gray-300'>ซัพพอร์ตฟังก์ชั่นทุกอย่างตามที่คุณต้องการ</p>
+                        <p className='mb-12 mt-4 text-gray-600 dark:text-gray-300'>ทางเราเข้าใจความต้องการของสถาบันหรือครูผู้สอนดี เเละเราพัฒนาระบบให้ตอบสนองการใช้งานของลูกค้าได้ครบครับ</p>
                         <div>
                             <button className='bg-[#1565c0] py-2 px-4 text-white rounded-full text-[16px]'>Get Started</button>
                         </div>
