3d124187e63d0d4a176b27db1f6c83d127460162
21 Tech Enterprise <59superrichsol@gmail.com>
2024-06-05 14:04:19 +0700

    done

--- app/pricing/Pricing.tsx
+++ app/pricing/Pricing.tsx
@@ -3,11 +3,14 @@ import PricePower from './PricePower'
 import ComparePlan from './ComparePlan'
 import FAQPricing from '../components/FAQ/FAQPricing';
 
+import GridThreeGadient from '../components/HomeNew/(NewTemplate)/GridThreeGadient';
 const Pricing = () => {
 
     return (
         <div className='container mx-auto pt-32'>
+             <GridThreeGadient/>
             <PricePower />
+           
             <ComparePlan />
             <h2 className="mt-12 mb-4 text-center text-3xl font-bold text-gray-800 dark:text-white md:text-4xl">คำถามที่พบบ่อย</h2>
             <FAQPricing />
