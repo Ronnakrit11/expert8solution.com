c7d277f08bcb02186fbf040ad4619915fce4ba77
21 Tech Enterprise <59superrichsol@gmail.com>
2024-06-10 10:39:23 +0700

    done

--- app/about/About.tsx
+++ app/about/About.tsx
@@ -108,7 +108,7 @@ const About = () => {
                           2021
                         </span>
                         <h3 className="mt-0.5 text-xl font-semibold text-gray-700 dark:text-white">
-                          Business Development ของ KPAuto Groups ซึ่งเป็น Dealer Top1 ชั้นนำของ Mazda , Mg , Suzuki
+                          Business Development ของ KPAuto Groups ซึ่งเป็น Dealer Top1 ชั้นนำของ Mazda , Mg เเละ Suzuki
                         </h3>
                         <p className="mt-2 text-gray-600 dark:text-gray-400">
                          
