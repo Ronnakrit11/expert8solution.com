cc4c1989a97db261a65d4a1b47a4f1c3900b1d2e
21 Tech Enterprise <59superrichsol@gmail.com>
2024-06-13 18:34:39 +0700

    doe

--- app/(services)/elearning/Solution.tsx
+++ app/(services)/elearning/Solution.tsx
@@ -8,9 +8,9 @@ const Solution = () => {
         <div className='bg-[#F8F9FA]'>
             <div className='text-center py-20 space-y-4'>
                 <p className='text-[16px]'>OUR <span className='font-bold'>FEATURES</span></p>
-                <p className='text-[30px] font-semibold'>ฟังก์ชั่นที่ตอบโจทย์</p>
+                <p className='text-[30px] font-semibold'>ฟังก์ชั่นพิเศษที่ตอบโจทย์</p>
                 <br />
-                <p className='text-[16px] text-[#6D7176]'> unde omnis iste natus error voluptatem accusantium doloremque rem aperiam.</p>
+                <p className='text-[16px] text-[#6D7176]'> รวบรวมทุกฟังก์ชั่นที่จะช่วยให้นักเรียนพัฒนาอย่างเต็มประสิทธิภาพ</p>
             </div>
             <div className='w-[90%] sm:w-[90%] md:w-[60%] mx-auto'>
                 <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-4'>
@@ -21,14 +21,14 @@ const Solution = () => {
                         className='w-[500px] h-auto' />                    </div>
                     <div className='flex justify-center items-center '>
                         <div className='text-black space-y-8'>
-                            <h1 className='text-[40px] sm:text-[40px] xl:text-[56px] font-bold'>Best Way to Make Online Payments</h1>
-                            <p className='text-[14px] sm:text-[14px] xl:text-[17px] text-[#6D7176]'>Maecenas tempus, tellus eget condimentum rhoncus quam semper libero sit amet adipiscing sem neque sed.</p>
+                            <h1 className='text-[40px] sm:text-[40px] xl:text-[56px] font-bold'>Pre test & Post test </h1>
+                            <p className='text-[14px] sm:text-[14px] xl:text-[17px] text-[#6D7176]'>ฟังก์ชั่นข้อสอบก่อนเรียนเเละหลังเรียนเเบบสามารถตั้งเวลาในการสอบได้</p>
                             <ul className='text-[17px] sm:text-[14px] xl:text-[16px] space-y-4 text-[#6D7176]'>
-                                <li className='flex items-center gap-1 sm:gap-1 xl:gap-2 '><LuCheckCircle className='text-[#98EFD2] w-[50px]' /> Aenean leo ligula porttitor eu consequat vitae.</li>
-                                <li className='flex items-center gap-1 sm:gap-1 xl:gap-2 '><LuCheckCircle className='text-[#98EFD2] w-[50px]' /> Aenean leo ligula porttitor eu consequat vitae.</li>
-                                <li className='flex items-center gap-1 sm:gap-1 xl:gap-2 '><LuCheckCircle className='text-[#98EFD2] w-[50px]' /> Aenean leo ligula porttitor eu consequat vitae.</li>
+                                <li className='flex items-center gap-1 sm:gap-1 xl:gap-2 '><LuCheckCircle className='text-[#98EFD2] w-[50px]' /> ครูผู้สอนสามารถเก็บข้อมูลคะเเนนสอบได้</li>
+                                <li className='flex items-center gap-1 sm:gap-1 xl:gap-2 '><LuCheckCircle className='text-[#98EFD2] w-[50px]' /></li>
+                                <li className='flex items-center gap-1 sm:gap-1 xl:gap-2 '><LuCheckCircle className='text-[#98EFD2] w-[50px]' /></li>
                             </ul>
-                            <button className='bg-primary text-white px-6 py-4 rounded-2xl'>Get Started</button>
+                            <button className='bg-primary text-white px-6 py-4 rounded-2xl'>Get Demo</button>
                             <br />
                         </div>
                         <br />
--- app/components/HomeNew/(NewTemplate)/LeftImageContent.tsx
+++ app/components/HomeNew/(NewTemplate)/LeftImageContent.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link'
 
 
 const LeftImageContent = () => {
@@ -11,12 +12,13 @@ const LeftImageContent = () => {
                         <Image width={500} height={500} alt='' src='/imageleft.webp' className='p-8' />
                     </div>
                     <div className='flex flex-col justify-center'>
-                        <h2 className='text-3xl font-bold text-gray-800 dark:text-white md:text-4xl xl:text-5xl'>เพราะเราเข้าใจในธุรกิจการเรียนออนไลน์</h2>
+                        <h2 className='text-3xl font-bold text-gray-800 dark:text-white md:text-3xl xl:text-3xl'>เพราะเราเข้าใจในธุรกิจการเรียนออนไลน์</h2>
                         <p className='mt-8 text-gray-600 dark:text-gray-300'>ซัพพอร์ตฟังก์ชั่นทุกอย่างตามที่คุณต้องการ</p>
-                        <p className='mb-12 mt-4 text-gray-600 dark:text-gray-300'>ทางเราเข้าใจความต้องการของสถาบันหรือครูผู้สอนดี เเละเราพัฒนาระบบให้ตอบสนองการใช้งานของลูกค้าได้ครบครับ</p>
+                        <p className='mb-12 mt-4 text-gray-600 dark:text-gray-300'>ทางเราเข้าใจความต้องการของสถาบันหรือครูผู้สอนดี เเละเราพัฒนาระบบให้ตอบสนองการใช้งานของลูกค้าได้ครบ</p>
                         <div>
-                            <button className='bg-[#1565c0] py-2 px-4 text-white rounded-full text-[16px]'>Get Started</button>
-                        </div>
+                        <Link href={"/elearning"}>
+                                        <button className='bg-[#1565c0] py-4 w-[200px] px-4 text-white rounded-full text-[16px]'>ดูรายละเอียดเพิ่มเติม</button>
+                                    </Link>                        </div>
                     </div>
                 </div>
             </div>
