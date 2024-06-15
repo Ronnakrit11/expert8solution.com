6e9fb135b88283d0786fd89092739e4086d74fba
9pyr <9pyra.s@gmail.com>
2024-06-15 20:24:21 +0700

    fixup

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,35 +1,26 @@
 "use client"
 import React, { useState } from "react"
 import Heading from "../utils/Heading"
-import Header from "../../components/Layout/Header"
-import About from "./About"
+import Header from "@/components/Layout/Header"
+
 import Footer from "../components/Footer"
 import AboutNew from "./AboutNew"
 
-type Props = {}
-
-const Page = (props: Props) => {
+const Page = () => {
   const [open, setOpen] = useState(false)
-  const [activeItem, setActiveItem] = useState(5)
   const [route, setRoute] = useState("Login")
 
   return (
     <div>
       <Heading
-        title={
-          "เกี่ยวกับเรา Expert8-Solution ทำซอฟเเวร์ พัฒนาเว็บไซต์ Marketing ครบวงจร"
-        }
-        description={
-          "บริษัท Expert8Solution จำกัด พัฒนาซอฟเเวร์ครบวงจร ระบบคอร์สเรียนออนไลน์ , ระบบ E-Commerce , ระบบจองโรงเเรม เเละการตลาดออนไลน์ทุกเเพลทฟอร์ม "
-        }
-        keywords={
-          "ทำคอร์สเรียนออนไลน์, ระบบคอร์สเรียนออนไลน์, ไอเดียคอร์สเรียนออนไลน์, การขายคอร์สออนไลน์"
-        }
+        title="เกี่ยวกับเรา Expert8-Solution ทำซอฟเเวร์ และพัฒนาเว็บไซต์ Marketing ครบวงจร"
+        description="บริษัท Expert8Solution จำกัด พัฒนาซอฟเเวร์ครบวงจร ระบบคอร์สเรียนออนไลน์ , ระบบ E-Commerce , ระบบจองโรงเเรม เเละการตลาดออนไลน์ทุกเเพลทฟอร์ม "
+        keywords="ทำคอร์สเรียนออนไลน์, ระบบคอร์สเรียนออนไลน์, ไอเดียคอร์สเรียนออนไลน์, การขายคอร์สออนไลน์"
       />
       <Header
         open={open}
         setOpen={setOpen}
-        activeItem={activeItem}
+        activeItem={5}
         setRoute={setRoute}
         route={route}
       />
