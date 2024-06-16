b317149ac10318f0d3fc00e82b22032914a63b0f
9pyr <9pyra.s@gmail.com>
2024-06-16 20:40:36 +0700

    mm2322 priority

--- app/components/Home/components/LandingPage.tsx
+++ app/components/Home/components/LandingPage.tsx
@@ -71,6 +71,7 @@ const LandingPage = () => {
               alt="ระบบคอร์สเรียนออนไลน์"
               src="/mm2322.webp"
               className="w-full"
+              priority
             />
           </div>
         </div>
